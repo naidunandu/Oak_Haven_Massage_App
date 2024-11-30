0bcb85dfc9b4a9909b1d0a544886328fa89fd679
nandininaidu <naidunandu237@gmail.com>
2024-11-30 10:54:48 +0530

    Create post

--- lib/view/feed/feed.dart
+++ lib/view/feed/feed.dart
@@ -25,6 +25,78 @@ class FeedView extends StatelessWidget {
           ),
         ],
       ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 23, vertical: 10),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                CircleAvatar(
+                  radius: 22,
+                  backgroundColor: Colors.amber.shade400,
+                  child: Image.asset(
+                    AppAssets.profileImg,
+                    height: 140,
+                    fit: BoxFit.contain,
+                  ),
+                ).paddingOnly(bottom: 12, right: 10),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween, // Distributes elements
+                      children: [
+                        Row(
+                          children: [
+                            const Text(
+                              "Emily Johnson",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ).paddingOnly(right: 5),
+                            const Icon(
+                              Icons.circle,
+                              color: CustomColors.secondary,
+                              size: 5,
+                            ).paddingOnly(right: 5),
+                            const Text(
+                              "10 mins",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w400,
+                                color: CustomColors.secondary,
+                              ),
+                            ).paddingOnly(right: 80),
+                          ],
+                        ),
+                        const Icon(Icons.more_horiz_rounded),
+                      ],
+                    ),
+                    const Text(
+                      "Massage Therapist",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 10,
+                        fontWeight: FontWeight.w500,
+                        color: CustomColors.secondary,
+                        letterSpacing: 0.1,
+                      ),
+                    ).paddingOnly(bottom: 12),
+                  ],
+                )
+              ],
+            ),
+            const Text(
+              "📚 Just conquered algorithms & data structures! 🎉 Time for a breather. Suggestions for a binge-worthy show? 🍿\n#StudyBreak #NetflixTime",
+              textAlign: TextAlign.start,
+              style: TextStyle(fontSize: 13, fontWeight: FontWeight.w400, letterSpacing: 0.1),
+            ).paddingOnly(bottom: 20),
+          ],
+        ),
+      ),
     );
   }
 }
